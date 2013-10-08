00efdca49cc9da56b2837d11fa8565f8944fa2b4
kelleykavanaugh <kelleykavanaugh@gmail.com>
2013-10-08 13:41:53 -0700

    changing hover effect on images, making splash page and setting index to default page view

--- wp-content/themes/kelleykavanaugh2/page6.php
+++ wp-content/themes/kelleykavanaugh2/page6.php
@@ -6,7 +6,6 @@
           <h1><?php the_title(); ?></h1>
           </div><!--/border-bottom-->
           <!-- Start The Loop -->
-          <?php query_posts( 'cat=6' ); ?>
           <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="border-bottom">
               <?php the_content() ?>
@@ -15,14 +14,6 @@
           <p>Sorry, no posts matched your criteria.</p>
           <?php endif; ?>
           <!-- End the Loop -->
-          <!--Start nav-->
-          <div class="pull-left">
-          <?php previous_posts_link( 'Newer' ); ?>
-          </div><!--/pull-left-->
-          <div class="pull-right">
-          <?php next_posts_link( 'Older' , $max_pages ); ?>
-          </div><!--/pull-right-->
-          <!--/ nav-->
           </div><!--/ .span -->
         </div><!--/ .row -->
       <?php get_footer(); ?>
---
+++ wp-content/themes/kelleykavanaugh2/splash.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title><?php wp_title('|', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Le styles -->
+    <link href="<?php bloginfo('stylesheet_url');?>" rel="stylesheet">
+
+    <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+
+    <?php wp_enqueue_script("jquery"); ?>
+    <?php wp_head(); ?>
+  </head>
+  <body>
+    <div class="banner-splash">
+    </div> <!--/banner-->
+      <div class="container-splash">
+            <?php /* Template Name: splash */ ?>
+<!-- HEADER END -->
+        <div class="row-fluid">
+          <div class="span4">
+            <a class="circle page2 splash" href="./web-design"></a>
+          </div><!--/span -->
+          <div class="span4">
+            <a class="circle page1 splash" href="./news"></a>
+          </div><!--/span -->
+          <div class="span4">
+            <a class="circle page3 splash" href="./costumes"></a>
+          </div><!--/span -->
+        </div><!--/row -->
+        <div class="row-fluid">
+          <div class="span2 offset2">
+            <a class="circle page4 splash" href="./management"></a>
+          </div><!--/span -->
+          <div class="span2 offset2">
+            <a class="circle page5 splash" href="./crafts"></a>
+          </div><!--/span -->
+          <div class="span2 offset2">
+            <a class="circle page6 splash" href="./contact"></a>
+          </div><!--/span -->
+        </div><!--/row -->
+      </div><!--/container-splash-->
+<!-- FOOTER -->
+            <div class="footer-splash">
+                <footer>
+                <div class="pull-left">
+                    <small><em><strong><a href="http://wordpress.org/">Wordpress</a></strong> Theme by <strong><a href="http://www.kelleykavanaugh.com/">Kelley Kavanaugh</a></strong>
+                </div><!--/. pull-left-->
+                <div class="pull-right">
+                    <small><em>&copy; 2013 <strong><?php bloginfo('name'); ?></div></strong></em></small>
+                </div><!--/ .pull-right -->
+                </footer>
+            </div><!--/footer-splash-->
+        <?php wp_footer(); ?>
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap-transition.js"></script>
+    <script src="../assets/js/bootstrap-alert.js"></script>
+    <script src="../assets/js/bootstrap-modal.js"></script>
+    <script src="../assets/js/bootstrap-dropdown.js"></script>
+    <script src="../assets/js/bootstrap-scrollspy.js"></script>
+    <script src="../assets/js/bootstrap-tab.js"></script>
+    <script src="../assets/js/bootstrap-tooltip.js"></script>
+    <script src="../assets/js/bootstrap-popover.js"></script>
+    <script src="../assets/js/bootstrap-button.js"></script>
+    <script src="../assets/js/bootstrap-collapse.js"></script>
+    <script src="../assets/js/bootstrap-carousel.js"></script>
+    <script src="../assets/js/bootstrap-typeahead.js"></script>
+    </body>
+</html>
