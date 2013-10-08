d237a7e4e02e5e84ec7de94c9fcc164dc46f6ca9
kelleykavanaugh <kelleykavanaugh@gmail.com>
2013-10-08 11:20:33 -0700

    replicated pages with different images

--- wp-content/themes/kelleykavanaugh2/page3.php
+++ wp-content/themes/kelleykavanaugh2/page3.php
@@ -1,12 +1,12 @@
-    <?php /* Template Name: Page 2 */ get_header(); ?>
-      <div class="circle page2"></div>
+    <?php /* Template Name: Page 3 */ get_header(); ?>
+      <div class="circle page3"></div>
         <div class="row-fluid">
           <div class="span12">
           <div class="border-bottom">
           <h1><?php the_title(); ?></h1>
           </div><!--/border-bottom-->
           <!-- Start The Loop -->
-          <?php query_posts( 'cat=2' ); ?>
+          <?php query_posts( 'cat=3' ); ?>
           <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="border-bottom">
               <?php the_content() ?>
--- wp-content/themes/kelleykavanaugh2/page6.php
+++ wp-content/themes/kelleykavanaugh2/page6.php
@@ -1,12 +1,12 @@
-    <?php /* Template Name: Page 2 */ get_header(); ?>
-      <div class="circle page2"></div>
+    <?php /* Template Name: Page 6 */ get_header(); ?>
+      <div class="circle page6"></div>
         <div class="row-fluid">
           <div class="span12">
           <div class="border-bottom">
           <h1><?php the_title(); ?></h1>
           </div><!--/border-bottom-->
           <!-- Start The Loop -->
-          <?php query_posts( 'cat=2' ); ?>
+          <?php query_posts( 'cat=6' ); ?>
           <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
             <div class="border-bottom">
               <?php the_content() ?>
