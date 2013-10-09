99de5b072409912212c6fe42827d8e4517dfb994
kelleykavanaugh <kelleykavanaugh@gmail.com>
2013-10-08 20:52:03 -0700

    cleaning up messy code, changing from images to icons on splash page

--- wp-content/themes/kelleykavanaugh2/splash.php
+++ wp-content/themes/kelleykavanaugh2/splash.php
@@ -12,7 +12,9 @@
     <!--[if lt IE 9]>
       <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-
+    <!-- font awesome -->
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+    <!-- /font awesome -->
     <?php wp_enqueue_script("jquery"); ?>
     <?php wp_head(); ?>
   </head>
@@ -24,24 +26,24 @@
       <div class="container-splash">
         <div class="row-fluid">
           <div class="span4">
-            <a class="circle page2 splash" href="./web-design"></a>
+            <a class="circle icon splash" href="./web-design"><i class="icon-keyboard"></i><h1 class="title">webdesign</h1></a>
           </div><!--/span -->
           <div class="span4">
-            <a class="circle page1 splash" href="./home"></a>
+            <a class="circle icon splash" href="./home"><i class="icon-heart-empty"></i><h1 class="title">home</h1></a>
           </div><!--/span -->
           <div class="span4">
-            <a class="circle page3 splash" href="./costumes"></a>
+            <a class="circle icon splash" href="./costumes"><i class="icon-suitcase"></i><h1 class="title">costumes</h1></a>
           </div><!--/span -->
         </div><!--/row -->
         <div class="row-fluid">
           <div class="span2 offset2">
-            <a class="circle page4 splash" href="./management"></a>
+            <a class="circle icon splash" href="./projects"><i class="icon-gears"></i><h1 class="title">projects</h1></a>
           </div><!--/span -->
           <div class="span2 offset2">
-            <a class="circle page5 splash" href="./crafts"></a>
+            <a class="circle icon splash" href="./crafts"><i class="icon-cut"></i><h1 class="title">crafts</h1></a>
           </div><!--/span -->
           <div class="span2 offset2">
-            <a class="circle page6 splash" href="./contact"></a>
+            <a class="circle icon splash" href="./contact"><i class="icon-inbox"></i><h1 class="title">contact</h1></a>
           </div><!--/span -->
         </div><!--/row -->
       </div><!--/container-splash-->
