623f6a35d3b8c90bc08d675bba6816f2a9205b3a
kelleykavanaugh <kelleykavanaugh@gmail.com>
2013-10-08 21:11:29 -0700

    trying to make the circle work correctly. deleted some unneeded link styles

--- wp-content/themes/kelleykavanaugh2/splash.php
+++ wp-content/themes/kelleykavanaugh2/splash.php
@@ -26,24 +26,24 @@
       <div class="container-splash">
         <div class="row-fluid">
           <div class="span4">
-            <a class="circle icon splash" href="./web-design"><i class="icon-keyboard"></i><h1 class="title">webdesign</h1></a>
+            <a class="circle icon splash" href="./web-design"><i class="icon-keyboard"></i><div class="title">webdesign</div></a>
           </div><!--/span -->
           <div class="span4">
-            <a class="circle icon splash" href="./home"><i class="icon-heart-empty"></i><h1 class="title">home</h1></a>
+            <a class="circle icon splash" href="./home"><i class="icon-heart-empty"></i><div class="title">home</div></a>
           </div><!--/span -->
           <div class="span4">
-            <a class="circle icon splash" href="./costumes"><i class="icon-suitcase"></i><h1 class="title">costumes</h1></a>
+            <a class="circle icon splash" href="./costumes"><i class="icon-suitcase"></i><div class="title">costumes</div></a>
           </div><!--/span -->
         </div><!--/row -->
         <div class="row-fluid">
           <div class="span2 offset2">
-            <a class="circle icon splash" href="./projects"><i class="icon-gears"></i><h1 class="title">projects</h1></a>
+            <a class="circle icon splash" href="./projects"><i class="icon-gears"></i><div class="title">projects</div></a>
           </div><!--/span -->
           <div class="span2 offset2">
-            <a class="circle icon splash" href="./crafts"><i class="icon-cut"></i><h1 class="title">crafts</h1></a>
+            <a class="circle icon splash" href="./crafts"><i class="icon-cut"></i><div class="title">crafts</div></a>
           </div><!--/span -->
           <div class="span2 offset2">
-            <a class="circle icon splash" href="./contact"><i class="icon-inbox"></i><h1 class="title">contact</h1></a>
+            <a class="circle icon splash" href="./contact"><i class="icon-inbox"></i><div class="title">contact</div></a>
           </div><!--/span -->
         </div><!--/row -->
       </div><!--/container-splash-->
