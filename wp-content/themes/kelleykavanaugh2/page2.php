b4fb97aab25e1e9dfe9447689c2147198c4b1fb0
kelleykavanaugh <kelleykavanaugh@gmail.com>
2013-10-09 21:26:32 -0700

    added block-text area to top of websites page, changed h1-h3 styles

--- wp-content/themes/kelleykavanaugh2/page2.php
+++ wp-content/themes/kelleykavanaugh2/page2.php
@@ -2,6 +2,21 @@
       <div class="circle icon"><i class="icon-keyboard"></i><h1 class="title"><?php the_title(); ?></h1></div>
         <div class="row-fluid">
           <div class="span12">
+            <div class="border-bottom">
+            </div><!--/border-bottom-->
+          <!-- Start The Loop -->
+          <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="border-bottom">
+              <div id="block-text">
+                <?php the_content() ?>
+              </div><!--/block-text-->
+            </div><!--/border-bottom-->
+          <?php endwhile; else: ?>
+          <p>Sorry, no posts matched your criteria.</p>
+          <?php endif; ?>
+          <!-- End the Loop -->
+            </div><!--/border-bottom-->
+
           <!-- Start The Loop -->
           <?php query_posts( 'cat=2' ); ?>
           <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
